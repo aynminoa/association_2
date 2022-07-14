86ed9dac55f0eedc354c4478fc7ea3494094c2f1
ayumi nakano <aynminoa@gmail.com>
2022-07-14 19:54:20 +0900

    アソシエーション2

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,30 @@
+class BlogsController < ApplicationController
+  def index
+    # @blogs = Blog.all
+  end
+
+  # def new
+  #   @blog = Blog.new
+  # end
+  
+  # def show
+
+  # end
+
+  # def edit
+
+  # end
+  
+  # def create
+
+  # end
+  
+  # def update
+
+  # end
+
+  # def destroy
+    
+  # end
+
+end
